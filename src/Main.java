810a96bdd97adc258e606688a5c1c4d182c8ce7b
Carlos Andrés Miranda Méndez <62572969+CarlosMM28@users.noreply.github.com>
2020-04-01 11:28:15 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,10 @@ public class Main {
         System.out.println("JAN CARLO DE LEÓN CIFUENTES");
         System.out.println("11 CALLE 2-43 ZONA 5 COLONIA LOS ENCINOS");
         System.out.println("33361433");
+        
+        System.out.println("Carlos Andrés Miranda Méndez");
+        System.out.println("1a Calle A, Colonia Villas del Magisterio Zona 9");
+        System.out.println("58385781");
     }
     
 }
